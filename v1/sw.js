445f4c75eda95447a3c1ba9e624d9fda87e4ad44
Louis Liu <47236598+reallouisliu@users.noreply.github.com>
2024-12-17 21:36:22 +0200

    Update function

--- v1/sw.js
+++ v1/sw.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = "my-app-v1";
 const urlsToCache = [
-  "/v1/",
-  "/v1/index.html",
-  "/v1/styles.css",
-  "/v1/assets/icons/icon-192x192.png",
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/assets/icons/icon-192x192.png",
   // ... 其他需要缓存的资源
 ];
 
 // 注册 service worker 时设置 scope
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/v1/sw.js", { scope: "/v1/" })
+    .register("/sw.js", { scope: "/v1/" })
     .then((registration) => {
       console.log("ServiceWorker registered with scope:", registration.scope);
     })
